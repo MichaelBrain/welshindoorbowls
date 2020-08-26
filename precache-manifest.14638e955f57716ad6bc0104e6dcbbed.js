02932a46623af74ef419111a978c1b01253e905e
Mike Brain <mikeybrain1302@gmail.com>
2020-08-26 16:10:09 +0100

    Updates

---
+++ precache-manifest.14638e955f57716ad6bc0104e6dcbbed.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea3584a2cad56f3a360bdbd8b07c496b",
+    "url": "/welshindoorbowls/index.html"
+  },
+  {
+    "revision": "16d977a43fe3f5fd5524",
+    "url": "/welshindoorbowls/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "cf0b9cc247168fa43ccd",
+    "url": "/welshindoorbowls/static/css/main.02411521.chunk.css"
+  },
+  {
+    "revision": "16d977a43fe3f5fd5524",
+    "url": "/welshindoorbowls/static/js/2.caded70a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/welshindoorbowls/static/js/2.caded70a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf0b9cc247168fa43ccd",
+    "url": "/welshindoorbowls/static/js/main.669a06c7.chunk.js"
+  },
+  {
+    "revision": "ba54b6ca9daf532a9f78",
+    "url": "/welshindoorbowls/static/js/runtime-main.efe95023.js"
+  },
+  {
+    "revision": "8eb77b00e0ef11f7932c5deb9af63b4c",
+    "url": "/welshindoorbowls/static/media/championsOfChampions.8eb77b00.jpg"
+  },
+  {
+    "revision": "b55a9acfadf7a6acfc32c793dd274d52",
+    "url": "/welshindoorbowls/static/media/championship.b55a9acf.jpg"
+  },
+  {
+    "revision": "2a489aecc3a693b3e0e271610629d922",
+    "url": "/welshindoorbowls/static/media/koCup.2a489aec.jpg"
+  },
+  {
+    "revision": "430ee8e8573ba4706e0eadcc52030699",
+    "url": "/welshindoorbowls/static/media/meritLeague.430ee8e8.jpg"
+  },
+  {
+    "revision": "5fce0d2b600b2981c1c164e9b97e3b1b",
+    "url": "/welshindoorbowls/static/media/o50Triples.5fce0d2b.jpg"
+  },
+  {
+    "revision": "ae7bb7040f9d1289da7d40301e024ebb",
+    "url": "/welshindoorbowls/static/media/o60Championship.ae7bb704.jpg"
+  },
+  {
+    "revision": "4ba9cfe067e217b49d62b82cec56a38f",
+    "url": "/welshindoorbowls/static/media/o60Fours.4ba9cfe0.jpg"
+  },
+  {
+    "revision": "edba54722b89628578a097b0814381b7",
+    "url": "/welshindoorbowls/static/media/o60Pairs.edba5472.jpg"
+  },
+  {
+    "revision": "4feeef65e5e854fb35c092e3857a2a29",
+    "url": "/welshindoorbowls/static/media/o60Singles.4feeef65.jpg"
+  },
+  {
+    "revision": "906aa925d7692ae375b3633732fa48b7",
+    "url": "/welshindoorbowls/static/media/openFours.906aa925.jpg"
+  },
+  {
+    "revision": "3d68af03cfbf3b5ece37154cbc92eb78",
+    "url": "/welshindoorbowls/static/media/openPairs.3d68af03.jpg"
+  },
+  {
+    "revision": "0b0ca2fbab353eff22c1c6d25ee5cb18",
+    "url": "/welshindoorbowls/static/media/openSingles.0b0ca2fb.jpg"
+  },
+  {
+    "revision": "f76490f50ebe2f4226fee363a0667fc5",
+    "url": "/welshindoorbowls/static/media/openTriples.f76490f5.jpg"
+  },
+  {
+    "revision": "e7d8bff0354aa819602a54a2c3ef6b51",
+    "url": "/welshindoorbowls/static/media/teamChampionship.e7d8bff0.jpg"
+  },
+  {
+    "revision": "57a7ddff5810d3d982a3909795bd0c2f",
+    "url": "/welshindoorbowls/static/media/u16Singles.57a7ddff.jpg"
+  },
+  {
+    "revision": "97e05e1c922de74cd5e35e7fffdadd21",
+    "url": "/welshindoorbowls/static/media/u18Singles.97e05e1c.jpg"
+  },
+  {
+    "revision": "dde4810886b6c05b70e362149ee0a1e4",
+    "url": "/welshindoorbowls/static/media/u25Singles.dde48108.jpg"
+  },
+  {
+    "revision": "4f279681562a18378b645cbe5ee776ad",
+    "url": "/welshindoorbowls/static/media/wiba9.4f279681.jpg"
+  }
+]);
