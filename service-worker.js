b86dfc27e0f546330c931ebdeffe06f897bd32bd
Mike Brain <mikeybrain1302@gmail.com>
2020-08-26 15:56:50 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/welshindoorbowls/precache-manifest.14638e955f57716ad6bc0104e6dcbbed.js"
+  "/welshindoorbowls/App.js/precache-manifest.8f74ba65e76d8535fdb9a588253671bc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/welshindoorbowls/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/welshindoorbowls/App.js/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
